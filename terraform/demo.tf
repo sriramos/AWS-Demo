fc0d2125b6d328099bce008100e61ca219d814af
sriramos <43912574+sriramos@users.noreply.github.com>
2022-07-23 23:27:55 -0700

    Update demo.tf

--- terraform/demo.tf
+++ terraform/demo.tf
@@ -1,1 +1,12 @@
+provider "aws" {
+  version = "~> 2.0"
+  region  = "us-west-2"
+}
 
+terraform {
+  backend "s3" {
+    bucket = "[YOUR_REMOTE_STATE_BUCKET_NAME]"
+    key    = "[YOUR_REMOTE_STATE_KEY]"
+    region = "us-west-2"
+  }
+}
